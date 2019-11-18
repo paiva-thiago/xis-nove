0fe2beed54631705aae43883ceac6f7652a499ab
Thiago Paiva <10606829+paiva-thiago@users.noreply.github.com>
2019-11-18 08:31:00 -0300

    using request-ip

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-// server.js
-// where your node app starts
+var express = require('express')
+var app = express()
+const requestIp = require('request-ip')
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
-  let ip = req.headers['x-forwarded-for'].split(',')[0]
+  let ip = requestIp.getClientIp(req)
   let lang = req.headers['accept-language']
   let nav = req.headers['user-agent']
   res.json({ipaddress: ip,language:lang,software:nav});
