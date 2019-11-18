3621587b9e7f2e8d884c1572e13e00d9c75ad59b
Thiago Paiva <10606829+paiva-thiago@users.noreply.github.com>
2019-11-18 09:31:07 -0300

    sem request-ip

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-const requestIp = require('request-ip')
+//const requestIp = require('request-ip')
 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
@@ -13,7 +13,13 @@ app.get("/", function (req, res) {
 
 
 app.get("/api/whoami", function (req, res) {
-  let ip = requestIp.getClientIp(req)
+  //let ip = requestIp.getClientIp(req)
+  let ip = req.headers['x-forwarded-for']
+  if(ip!=undefined){
+    ip = ip.split(',')[0]
+  }else{
+    ip = 'falha ao obter ip'
+  }
   let lang = req.headers['accept-language']
   let nav = req.headers['user-agent']
   res.json({ipaddress: ip,language:lang,software:nav});
